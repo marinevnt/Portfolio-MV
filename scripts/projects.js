20a9713cd116d89803947c0f6943cdb61793f558
Marine Veniat <marine.veniat@hotmail.fr>
2023-12-05 19:26:49 +0100

    setting up the responsive part, adding links to github projects

--- scripts/projects.js
+++ scripts/projects.js
@@ -5,6 +5,22 @@ const modalTitle = document.getElementById('modalTitle');
 const modalImage = document.querySelector('.modal-image');
 const modalDescription = document.querySelector('.modal-description');
 
+const modalLinkGithub = document.querySelector('.github-link');
+const modalLinkwebsite = document.querySelector('.link-website');
+
+const siteWebLink = document.createElement('a');
+//siteWebLink.target = '_blank';
+siteWebLink.innerText = 'Voir le site';
+modalLinkwebsite.appendChild(siteWebLink);
+
+const githubLink = document.createElement('a');
+githubLink.target = '_blank';
+const githubIcon = document.createElement('i');
+githubIcon.className = 'fab fa-github';
+githubLink.appendChild(githubIcon);
+githubLink.appendChild(document.createTextNode(' Voir sur GitHub'));
+modalLinkGithub.appendChild(githubLink);
+
 
 for (let i = 0; i < projects.length; i++) {
     const project = projects[i];  
@@ -21,11 +37,18 @@ for (let i = 0; i < projects.length; i++) {
         </div>
     `;
 
+
     card.addEventListener('click', function () {
         // On met à jour le titre et l'image de la modale avec les informations du projet
         modalTitle.innerText = project.name;
         modalImage.src = project.imagewebsite;
         modalDescription.textContent = project.description;
+
+        githubLink.hidden = (project.gitlink === undefined);
+        githubLink.href = project.gitlink;
+
+        siteWebLink.hidden = (project.website === undefined);
+        siteWebLink.href = project.website;
 
         // On ouvre la modale à l'aide de Bootstrap
         const modal = new bootstrap.Modal(document.getElementById('projectModal'));
--- scripts/projectsData.js
+++ scripts/projectsData.js
@@ -3,37 +3,45 @@ const projects = [
     name: "Création la page d'accueil d'une agence de voyage avec HTML & CSS",
     description: "L'objectif de ce projet est de développer un site Internet qui permet aux usagers de trouver des hébergements et des activités dans la ville de leur choix.\nL'interface est créée avec du code HTML et CSS.",
     image: "./assets/booki.JPG",
-    imagewebsite: "./assets/booki-entirepage.png"
+    imagewebsite: "./assets/booki-entirepage.png",
   },
   {
     name: "Amélioration de l'interface d'un site mobile avec des animations CSS",
     description: "La mission est de développer un site “mobile first” qui répertorie les menus de restaurants gastronomiques. Le site doit être responsive (mobile, tablette et desktop) et en “mobile first”. \nCe projet met en avant la maîtrise de l'implémentation des animations CSS et l'utilisation de SASS.",
     image: "./assets/ohmyfood.JPG",
-    imagewebsite: "./assets/ohmyfood-entirepage.png"
+    imagewebsite: "./assets/ohmyfood-entirepage.png",
+    website: "https://marinevnt.github.io/Ohmyfood---Openclassrooms/",
+    gitlink: "https://github.com/marinevnt/Ohmyfood---Openclassrooms.git"
   },
   {
     name: "Création d'une application web de location immobilière avec React",
     description: "Ce projet tend à développer un site de location d'appartement en utilisant React. La mission est de créer l’ensemble de l’application, les composants React et des routes React Router. \nSass a été utilisé pour générer le style CSS du site web.",
     image: "./assets/kasa.jpg",
-    imagewebsite: "./assets/kasapage-entirepage.png"
+    imagewebsite: "./assets/kasapage-entirepage.png",
+    website: "https://marinevnt.github.io/Kasa----Openclassrooms/",
+    gitlink: "https://github.com/marinevnt/Kasa----Openclassrooms.git"
   },
   {
     name: "Optimisation du référencement d'un site de photographe",
     description: "Ce projet à pour but d'optimiser le référencement SEO d'un site de photographe. Les images, le code et la structure du site doivent être optimisés, afin d'offrir un meilleur référencement et une expérience client plus agréable. \nUn rapport détaillé sur l'optimisation des performances a été rédigé. \nDifférents outils ont été manipulés, notamment Lighthouse, GTmetrix, Wave et Google Rich Results Test.",
     image: "./assets/ninacarducci.JPG",
-    imagewebsite: "./assets/ninacarducci-entirepage.png"
+    imagewebsite: "./assets/ninacarducci-entirepage.png",
+    website: "https://marinevnt.github.io/Nina-Carducci--OpenClassrooms/",
+    gitlink: "https://github.com/marinevnt/Nina-Carducci--OpenClassrooms.git"
   },
   {
     name: "Débogage du site d'une agence d'événementiel",
     description: "L'objectif de ce projet est de corriger les différents bugs du site internet d'une agence d'événementiel. \nOutre le débogage, les objectifs sont également d'améliorer le site, de s'entraîner à la rédaction de tests unitaires, ainsi que rédiger un cahier de recette qui contient les différents scénarios vérifiant que le site fonctionne correctement. \nLe site utilise React et SASS.",
     image: "./assets/77events.JPG",
-    imagewebsite: "./assets/77events-entirepage.png"
+    imagewebsite: "./assets/77events-entirepage.png",
+    gitlink: "https://github.com/marinevnt/77events--Openclassrooms.git"
   },
   {
     name: "Implémentation du front-end d'une application bancaire avec React",
     description: "La mission est d'implémenter le front-end d'une application bancaire. \nIl faut pour cela créer une application web complète et responsive avec React et utiliser Redux pour gérer le state de l'ensemble de l'application. \nCe projet a également mis l'accent sur les bonnes pratiques du Green Code à respecter notamment au niveau des images et des composants réutilisables.",
     image: "./assets/argentbank.jpg",
-    imagewebsite: "./assets/argentbank-entirepage.png"
+    imagewebsite: "./assets/argentbank-entirepage.png",
+    gitlink: "https://github.com/marinevnt/Argent-Bank--Openclassrooms.git"
   },
 ];
 
